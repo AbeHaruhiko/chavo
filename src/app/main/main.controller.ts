b7e025c76f2ab81d389402209486a7f1313f85d7
AbeHaruhiko <caliconography@gmail.com>
2015-06-09 23:28:52 +0900

    [modify] media objectを使うよう修正。

--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -18,7 +18,7 @@ module chavo {
     constructor (public $scope: IMainScope) {
       var awesomes = [
         {
-          'description': '中華料理屋さんで。\n私「何食べたい？」',
+          'description': '中華料理屋さんで。\n私「何食べたい？」\n子「ベイマックス！！！！！！！」\n私「えっ」\n\n子が指差す先を見ると・・・・酢豚の「パイナップル」（笑語呂がにてるっちゃにてるｗｗｗ',
           'author': 'あべさんのお子さん',
           'age': 2.6,
           'icon': 'angular.png',
