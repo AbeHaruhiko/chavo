4a191892c5858147afe0ce1df07d82a9b06f8c92
AbeHaruhiko <caliconography@gmail.com>
2015-06-09 22:29:00 +0900

    thumbnail調整

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,41 @@
+var chavo;
+(function (chavo) {
+    'use strict';
+    var Voice = (function () {
+        function Voice(description, author, age, icon, createdAt) {
+            this.description = description;
+            this.author = author;
+            this.age = age;
+            this.icon = icon;
+            this.createdAt = createdAt;
+        }
+        return Voice;
+    })();
+    var MainCtrl = (function () {
+        function MainCtrl($scope) {
+            var _this = this;
+            this.$scope = $scope;
+            this.voices = new Array();
+            var awesomes = [
+                {
+                    'description': 'ぱぱ！！',
+                    'author': 'あべさんのお子さん',
+                    'age': 2.6,
+                    'icon': 'angular.png',
+                    'createdAt': '2015-06-02'
+                }, {
+                    'description': 'ぱぱ！！',
+                    'author': 'あべさんのお子さん',
+                    'age': 2.6,
+                    'icon': 'ic_hal.jpg',
+                    'createdAt': '2015-06-02'
+                }
+            ];
+            awesomes.forEach(function (awesome) {
+                _this.voices.push(awesome);
+            });
+        }
+        return MainCtrl;
+    })();
+    chavo.MainCtrl = MainCtrl;
+})(chavo || (chavo = {}));
---
+++ src/app/main/main.controller.ts
@@ -0,0 +1,41 @@
+module chavo {
+  'use strict';
+
+  class Voice {
+    constructor(public description: string, public author: string, public age: number, public icon: string, public createdAt: string) {
+    }
+  }
+
+  interface IMainScope extends ng.IScope {
+    voices: Voice[]
+  }
+
+  export class MainCtrl {
+
+    voices = new Array<Voice>();
+
+    /* @ngInject */
+    constructor (public $scope: IMainScope) {
+      var awesomes = [
+        {
+          'description': 'ぱぱ！！',
+          'author': 'あべさんのお子さん',
+          'age': 2.6,
+          'icon': 'angular.png',
+          'createdAt': '2015-06-02'
+        },{
+          'description': 'ぱぱ！！',
+          'author': 'あべさんのお子さん',
+          'age': 2.6,
+          'icon': 'ic_hal.jpg',
+          'createdAt': '2015-06-02'
+        }
+    ];
+
+      awesomes.forEach((awesome: Voice) => {
+        this.voices.push(awesome);
+      });
+    }
+  }
+
+}
