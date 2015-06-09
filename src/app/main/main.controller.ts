ce66fd3e6c6bb83127f9f177381d54ba500343be
AbeHaruhiko <caliconography@gmail.com>
2015-06-09 22:48:47 +0900

    [modify] descriptionの改行(\n)が表示に反映されるよう、CSS(pre-line)をセット。

--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -18,7 +18,7 @@ module chavo {
     constructor (public $scope: IMainScope) {
       var awesomes = [
         {
-          'description': 'ぱぱ！！',
+          'description': '中華料理屋さんで。\n私「何食べたい？」',
           'author': 'あべさんのお子さん',
           'age': 2.6,
           'icon': 'angular.png',
