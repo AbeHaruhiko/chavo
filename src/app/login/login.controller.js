5533cd9736a7cdc3311b468d742e3b45d502f205
AbeHaruhiko <caliconography@gmail.com>
2015-06-11 22:15:59 +0900

    [modify] state

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -2,9 +2,30 @@ var chavo;
 (function (chavo) {
     'use strict';
     var LoginController = (function () {
-        function LoginController($scope) {
+        function LoginController($scope, $state) {
             this.$scope = $scope;
+            this.$state = $state;
         }
+        LoginController.prototype.logIn = function (form) {
+            AuthService.logIn(form, {
+                success: function (user) {
+                    this.$scope.$apply(function () {
+                        $scope.currentUser = user;
+                        AuthService.currentUser = user;
+                        if (!user.get('emailVerified')) {
+                            $state.go('login');
+                            return;
+                        }
+                        $state.go('main');
+                    });
+                },
+                error: function (user, error) {
+                    console.log("Unable to login:  " + error.code + " " + error.message);
+                    $location.path('/login');
+                }
+            });
+        };
+        ;
         LoginController.prototype.logOut = function () {
             Parse.User.logOut();
         };
--- src/app/login/login.controller.ts
+++ src/app/login/login.controller.ts
@@ -7,9 +7,30 @@ module chavo {
   export class LoginController {
 
     /* @ngInject */
-    constructor (public $scope: IMainScope) {
+    constructor (public $scope: IMainScope, public $state: ng.ui.IState) {
 
     }
+
+    logIn(form) {
+          AuthService.logIn(form, {
+            success: function(user) {
+              this.$scope.$apply(function() {
+                $scope.currentUser = user;
+                AuthService.currentUser = user;
+                if (!user.get('emailVerified')) {
+                  $state.go('login');
+                  return;
+                }
+
+                $state.go('main');
+              });
+            },
+            error: function(user, error) {
+              console.log("Unable to login:  " + error.code + " " + error.message);
+              $location.path('/login');
+            }
+          });
+        };
 
     logOut() {
       Parse.User.logOut();
