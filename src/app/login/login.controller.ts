5d81b854bedcbdc6c01eb7d18c5d9504ba5d63b4
AbeHaruhiko <caliconography@gmail.com>
2015-06-12 13:46:52 +0900

    [modify] 型アノテーション追加など

--- src/app/login/login.controller.ts
+++ src/app/login/login.controller.ts
@@ -7,26 +7,26 @@ module chavo {
   export class LoginController {
 
     /* @ngInject */
-    constructor (public $scope: IMainScope, public $state: ng.ui.IStateService, public $location, public AuthService: AuthService) {
+    constructor (public $scope: IMainScope, public $state: ng.ui.IStateService, public $location: ng.ILocationService, public AuthService: AuthService) {
 
     }
 
-    signUp(form) {
+    signUp(form: { name: string; password: string; }) {
       this.AuthService.signUp(form, {
-        success: (user) => {
+        success: (user: Parse.User) => {
           this.$scope.$apply(() => {
             this.$state.go('login');
           });
         },
-        error: (user, error) => {
-          alert("Unable to sign up:  " + error.code + " " + error.message);
+        error: (user: Parse.User, error: Parse.Error) => {
+          alert('Unable to sign up:  ' + error.code + ' ' + error.message);
         }
       });
     }
 
-    logIn(form) {
+    logIn(form: { name: string; password: string; }) {
       this.AuthService.logIn(form, {
-        success: user => {
+        success: (user: Parse.User) => {
           this.$scope.$apply(() => {
             if (!user.get('emailVerified')) {
               this.$state.go('main');
@@ -36,8 +36,8 @@ module chavo {
             this.$state.go('main');
           });
         },
-        error: (user, error) => {
-          console.log("Unable to login:  " + error.code + " " + error.message);
+        error: (user:Parse.User, error: Parse.Error) => {
+          console.log('Unable to login:  ' + error.code + ' ' + error.message);
           this.$location.path('/login');
         }
       });
