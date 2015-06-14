b6f973ef590471d8b32f281206fa6a46d54bfb5c
AbeHaruhiko <caliconography@gmail.com>
2015-06-14 21:48:08 +0900

    [modify] VoiceのParseからの取得実装中

--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -20,25 +20,38 @@ module chavo {
 
     /* @ngInject */
     constructor (public $scope: IMainScope) {
-      var awesomes = [
-        {
-          'description': '中華料理屋さんで。\n私「何食べたい？」\n子「ベイマックス！！！！！！！」\n私「えっ」\n\n子が指差す先を見ると・・・・酢豚の「パイナップル」（笑語呂がにてるっちゃにてるｗｗｗ',
-          'author': 'あべさんのお子さん',
-          'age': 2.6,
-          'icon': 'angular.png',
-          'createdAt': '2015-06-02'
-        }, {
-          'description': 'ぱぱ！！',
-          'author': 'あべさんのお子さん',
-          'age': 2.6,
-          'icon': 'ic_hal.jpg',
-          'createdAt': '2015-06-02'
-        }
-    ];
 
-      awesomes.forEach((awesome: Voice) => {
-        this.voices.push(awesome);
-      });
+
+      var Voice = Parse.Object.extend("Voice");
+  		var query = new Parse.Query(Voice);
+  		query.descending("createdAt");
+  		query.find({
+  		  success: (results: Voice[]) => {
+          this.$scope.$apply(() => {
+            this.voices = results;
+          });
+  		  },
+  		  error: function(error) {
+  		    alert("Error: " + error.code + " " + error.message);
+  		  }
+  		});
+
+
+      // this.voices = [
+      //   {
+      //     'description': '中華料理屋さんで。\n私「何食べたい？」\n子「ベイマックス！！！！！！！」\n私「えっ」\n\n子が指差す先を見ると・・・・酢豚の「パイナップル」（笑語呂がにてるっちゃにてるｗｗｗ',
+      //     'author': 'あべさんのお子さん',
+      //     'age': 2.6,
+      //     'icon': 'angular.png',
+      //     'createdAt': '2015-06-02'
+      //   }, {
+      //     'description': 'ぱぱ！！',
+      //     'author': 'あべさんのお子さん',
+      //     'age': 2.6,
+      //     'icon': 'ic_hal.jpg',
+      //     'createdAt': '2015-06-02'
+      //   }
+      // ];
     }
   }
 
