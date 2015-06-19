aa502a2c8c140f6442fe790e3e72e34412df64d3
AbeHaruhiko <caliconography@gmail.com>
2015-06-19 18:58:31 +0900

    [add] 月齢表示実装中

--- src/app/settings/content/settings.children.controller.js
+++ src/app/settings/content/settings.children.controller.js
@@ -1,14 +1,31 @@
 var chavo;
 (function (chavo) {
     'use strict';
+    var Child = (function () {
+        function Child(nickName, birthday, sex, age) {
+            this.nickName = nickName;
+            this.birthday = birthday;
+            this.sex = sex;
+            this.age = age;
+        }
+        return Child;
+    })();
     var SettingsChildrenController = (function () {
         function SettingsChildrenController($scope, $rootScope) {
+            // this.children = [
+            //   { nickName: 'もも', birthDay: '2012/11/26', sex: 2},
+            //   { nickName: 'あお'},
+            // ];
             this.$scope = $scope;
             this.$rootScope = $rootScope;
-            this.children = [
-                { nickName: 'もも', birthDay: '2012/11/26', sex: 2 },
-                { nickName: 'あお' },
-            ];
+            this.children = new Array();
+            this.children.push(new Child('もも', moment('2012/11/26', 'YYYY/MM/DD').toDate(), 2));
+            this.children.push(new Child('あお', null, null));
+            this.children.forEach(function (child) {
+                var years = moment().diff(moment(child.birthday), 'years');
+                var months = moment().diff(moment(child.birthday), 'months');
+                child.age = years + '歳' + months + 'ヶ月';
+            });
         }
         return SettingsChildrenController;
     })();
--- src/app/settings/content/settings.children.controller.ts
+++ src/app/settings/content/settings.children.controller.ts
@@ -1,19 +1,42 @@
 module chavo {
   'use strict';
 
+  class Child {
+    constructor(public nickName: string,
+      public birthday: Date,
+      public sex: number,
+      public age?: string) {
+    }
+  }
+
+
   interface ISettingsChildren extends ng.IScope {
+    children: Child[];
   }
 
   export class SettingsChildrenController {
+
+    children = new Array<Child>();
+
     /* @ngInject */
     constructor (public $scope: ISettingsChildren,
       public $rootScope: IChavoRootScope) {
-    }
 
-    children = [
-      { nickName: 'もも', birthDay: '2012/11/26', sex: 2},
-      { nickName: 'あお'},
-    ];
+        // this.children = [
+        //   { nickName: 'もも', birthDay: '2012/11/26', sex: 2},
+        //   { nickName: 'あお'},
+        // ];
+
+        this.children.push(new Child('もも', moment('2012/11/26', 'YYYY/MM/DD').toDate(), 2));
+        this.children.push(new Child('あお', null, null));
+
+        this.children.forEach((child: Child) => {
+          let years = moment().diff(moment(child.birthday), 'years');
+          let months = moment().diff(moment(child.birthday), 'months');
+          child.age = years + '歳' + months + 'ヶ月';
+        });
+
+    }
 
 
   }
