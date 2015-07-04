cfc286259ceab9f4c4119a36f7c080785bdb76e4
AbeHaruhiko <caliconography@gmail.com>
2015-07-04 22:43:50 +0900

    [modify] 誕生日未登録時にNaNと表示されるのを修正

--- src/app/main/compose/main.compose.controller.js
+++ src/app/main/compose/main.compose.controller.js
@@ -18,10 +18,12 @@ var chavo;
                 }
             }).then(function (results) {
                 results.forEach(function (parseChild) {
-                    var years = '' + moment().diff(moment(parseChild.get('birthday')), 'years');
-                    var months = '' + (moment().diff(moment(parseChild.get('birthday')), 'months') - (12 * +years));
+                    if (parseChild.get('birthday')) {
+                        var years = '' + moment().diff(moment(parseChild.get('birthday')), 'years');
+                        var months = '' + (moment().diff(moment(parseChild.get('birthday')), 'months') - (12 * +years));
+                    }
                     _this.$scope.$apply(function () {
-                        _this.children.push(new chavo.Child(parseChild.get('dispOrder'), parseChild.get('nickName'), parseChild.get('birthday'), parseChild.get('gender'), years ? years : null, months ? months : null));
+                        _this.children.push(new chavo.Child(parseChild.get('dispOrder'), parseChild.get('nickName'), parseChild.get('birthday'), parseChild.get('gender'), years ? years : null, months ? months : null, !parseChild.get('birthday')));
                     });
                 });
             });
