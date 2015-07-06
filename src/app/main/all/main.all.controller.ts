4e879240343ec40ed035e88bbfe3b6d57e55361c
AbeHaruhiko <caliconography@gmail.com>
2015-07-06 22:07:49 +0900

    [add] 投稿に日付表示

--- src/app/main/all/main.all.controller.ts
+++ src/app/main/all/main.all.controller.ts
@@ -11,6 +11,7 @@ module chavo {
       var ParseVoice = Parse.Object.extend('Voice');
   		var query = new Parse.Query(ParseVoice);
   		query.descending('createdAt');
+
   		query.find({
   		  success: (results: Parse.Object[]) => {
 
@@ -22,8 +23,8 @@ module chavo {
               voice.get('author'),
               (voice.get('ageYears') && voice.get('ageMonths')) ? (voice.get('ageYears') + '歳' + voice.get('ageMonths') + 'ヶ月') : '',
               voice.get('gender') == 0 ? '男の子' : voice.get('gender') == 1 ? '女の子' : '',
-              null,   // TODO
-              voice.createdAt    // TODO
+              null,
+              moment(voice.createdAt).format('YYYY/MM/DD').toString()
             ));
           });
 
--- src/app/main/myposts/main.myposts.controller.js
+++ src/app/main/myposts/main.myposts.controller.js
@@ -6,15 +6,16 @@ var chavo;
             var _this = this;
             this.$scope = $scope;
             this.voices = new Array();
-            var Voice = Parse.Object.extend('Voice');
-            var query = new Parse.Query(Voice);
+            var ParseVoice = Parse.Object.extend('Voice');
+            var query = new Parse.Query(ParseVoice);
             query.descending('createdAt');
             query.equalTo('user', Parse.User.current());
             query.find({
                 success: function (results) {
-                    _this.$scope.$apply(function () {
-                        _this.voices = results;
+                    results.forEach(function (voice) {
+                        _this.voices.push(new chavo.Voice(voice.get('description'), voice.get('author'), (voice.get('ageYears') && voice.get('ageMonths')) ? (voice.get('ageYears') + '歳' + voice.get('ageMonths') + 'ヶ月') : '', voice.get('gender') == 0 ? '男の子' : voice.get('gender') == 1 ? '女の子' : '', null, moment(voice.createdAt).format('YYYY/MM/DD').toString()));
                     });
+                    _this.$scope.$apply();
                 },
                 error: function (error) {
                     alert('Error: ' + error.code + ' ' + error.message);
