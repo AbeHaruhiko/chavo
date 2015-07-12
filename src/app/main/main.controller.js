87e57dc516c9ae5112ba3bea9ab451e02ae72093
AbeHaruhiko <caliconography@gmail.com>
2015-07-12 16:35:46 +0900

    [modify] main.controllerのいらない処理を削除。

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,23 +2,7 @@ var chavo;
 (function (chavo) {
     'use strict';
     var MainController = (function () {
-        function MainController($scope) {
-            var _this = this;
-            this.$scope = $scope;
-            this.voices = new Array();
-            var Voice = Parse.Object.extend('Voice');
-            var query = new Parse.Query(Voice);
-            query.descending('createdAt');
-            query.find({
-                success: function (results) {
-                    _this.$scope.$apply(function () {
-                        _this.voices = results;
-                    });
-                },
-                error: function (error) {
-                    alert('Error: ' + error.code + ' ' + error.message);
-                }
-            });
+        function MainController() {
         }
         return MainController;
     })();
--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -2,48 +2,9 @@ module chavo {
   'use strict';
 
   export interface IMainScope extends ng.IScope {
-    voices: Voice[]
   }
 
   export class MainController {
-
-    voices = new Array<Voice>();
-
-    /* @ngInject */
-    constructor (public $scope: IMainScope) {
-
-
-      var Voice = Parse.Object.extend('Voice');
-  		var query = new Parse.Query(Voice);
-  		query.descending('createdAt');
-  		query.find({
-  		  success: (results: Voice[]) => {
-          this.$scope.$apply(() => {
-            this.voices = results;
-          });
-  		  },
-  		  error: function(error: Parse.Error) {
-  		    alert('Error: ' + error.code + ' ' + error.message);
-  		  }
-  		});
-
-
-      // this.voices = [
-      //   {
-      //     'description': '中華料理屋さんで。\n私「何食べたい？」\n子「ベイマックス！！！！！！！」\n私「えっ」\n\n子が指差す先を見ると・・・・酢豚の「パイナップル」（笑語呂がにてるっちゃにてるｗｗｗ',
-      //     'author': 'あべさんのお子さん',
-      //     'age': 2.6,
-      //     'icon': 'angular.png',
-      //     'createdAt': '2015-06-02'
-      //   }, {
-      //     'description': 'ぱぱ！！',
-      //     'author': 'あべさんのお子さん',
-      //     'age': 2.6,
-      //     'icon': 'ic_hal.jpg',
-      //     'createdAt': '2015-06-02'
-      //   }
-      // ];
-    }
   }
 
 }
