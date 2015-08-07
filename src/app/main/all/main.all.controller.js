475898262fe0607c86cbe979df6d59a020886e5e
AbeHaruhiko <caliconography@gmail.com>
2015-08-07 22:58:21 +0900

    [modify] アイコンロード実装中

--- src/app/main/all/main.all.controller.js
+++ src/app/main/all/main.all.controller.js
@@ -8,17 +8,27 @@ var chavo;
             this.voices = new Array();
             var ParseVoice = Parse.Object.extend('Voice');
             var query = new Parse.Query(ParseVoice);
+            var parseVoices;
             query.descending('createdAt');
             query.find({
                 success: function (results) {
-                    results.forEach(function (voice) {
-                        _this.voices.push(new chavo.Voice(voice.get('description'), voice.get('author'), (voice.get('ageYears') && voice.get('ageMonths')) ? (voice.get('ageYears') + '歳' + voice.get('ageMonths') + 'ヶ月') : '', voice.get('gender') === 0 ? '男の子' : voice.get('gender') === 1 ? '女の子' : '', voice.get('user').get('iconUrl'), moment(voice.createdAt).format('YYYY/MM/DD').toString()));
-                    });
-                    _this.$scope.$apply();
                 },
                 error: function (error) {
                     alert('Error: ' + error.code + ' ' + error.message);
                 }
+            }).then(function (results) {
+                parseVoices = results;
+                var promises = [];
+                results.forEach(function (voice) {
+                    promises.push(voice.get('user').fetch());
+                });
+                return Parse.Promise.when(promises);
+            })
+                .then(function () {
+                parseVoices.forEach(function (voice) {
+                    _this.voices.push(new chavo.Voice(voice.get('description'), voice.get('author'), (voice.get('ageYears') && voice.get('ageMonths')) ? (voice.get('ageYears') + '歳' + voice.get('ageMonths') + 'ヶ月') : '', voice.get('gender') === 0 ? '男の子' : voice.get('gender') === 1 ? '女の子' : '', voice.get('user').get('username'), voice.get('user').get('iconUrl'), moment(voice.createdAt).format('YYYY/MM/DD').toString()));
+                });
+                _this.$scope.$apply();
             });
         }
         return MainAllController;
