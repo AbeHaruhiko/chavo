5edd61c5a23b59233538cf09f0447955cced40a6
AbeHaruhiko <caliconography@gmail.com>
2015-09-23 21:32:33 +0900

    [modify] profileの画像保存は送信ボタン押下時に変更。

--- src/app/settings/content/profile/settings.profile.controller.js
+++ src/app/settings/content/profile/settings.profile.controller.js
@@ -2,48 +2,27 @@ var chavo;
 (function (chavo) {
     'use strict';
     var SettingsProfileController = (function () {
-        function SettingsProfileController($scope, $rootScope) {
-            var _this = this;
+        function SettingsProfileController($scope, $rootScope, cfpLoadingBar) {
             this.$scope = $scope;
             this.$rootScope = $rootScope;
+            this.cfpLoadingBar = cfpLoadingBar;
             this.showIcon = false;
+            this.disableInput = false;
             this.profile = new chavo.Profile($rootScope.currentUser.getUsername(), $rootScope.currentUser.getEmail(), null, $rootScope.currentUser.get('iconUrl'), $rootScope.currentUser.get('likes'));
             var fileSelector = angular.element('#photo-selector');
             fileSelector.fileinput({
-                'uploadUrl': 'dummy',
-                'dropZoneEnabled': false,
-                'showCaption': false,
-                'showPreview': true,
-                'browseClass': 'photo-browse-btn btn btn-primary btn-outline',
-                'browseLabel': '画像選択'
-            });
-            fileSelector.on('filepreupload', function (event, data, previewId, index) {
-                if (data.files.length > 0) {
-                    var file = data.files[0];
-                    var name = 'photo.jpg';
-                    var parseFile = new Parse.File(name, file);
-                    parseFile.save().then(function () {
-                        _this.$rootScope.currentUser.set('icon', parseFile);
-                        _this.$rootScope.currentUser.set('iconUrl', parseFile.url());
-                        return _this.$rootScope.currentUser.save();
-                    }).then(function () {
-                        var profilePhoto = _this.$rootScope.currentUser.get('icon');
-                        $scope.$apply(function () {
-                            _this.profile.icon = profilePhoto.url();
-                            _this.showIcon = true;
-                        });
-                        fileSelector.fileinput('clear');
-                    }, function (error) {
-                        console.error(error);
-                    });
-                }
-            });
-            fileSelector.on('fileuploaderror', function (event, data, previewId, index) {
-                angular.element('.file-error-message').css('display', 'none');
-                console.error('file upload error. (this is expected results. please ignore.)');
+                dropZoneEnabled: false,
+                showCaption: false,
+                showUpload: false,
+                showPreview: true,
+                browseClass: 'photo-browse-btn btn btn-primary btn-outline',
+                browseLabel: '画像選択'
             });
         }
         SettingsProfileController.prototype.saveProfile = function () {
+            var _this = this;
+            this.disableInput = true;
+            this.cfpLoadingBar.start();
             var profInput = {
                 username: this.profile.username,
                 email: this.profile.email
@@ -51,12 +30,42 @@ var chavo;
             if (this.profile.password) {
                 profInput.password = this.profile.password;
             }
-            this.$rootScope.currentUser.save(profInput)
-                .then(function (user) {
-                console.log('ほぞんしました');
-            }, function (error) {
-                console.log('Error: ' + error.code + ' ' + error.message);
-            });
+            var fileSelector = angular.element('#photo-selector');
+            if (fileSelector[0].files.length > 0) {
+                var file = fileSelector[0].files[0];
+                var name = 'photo.jpg';
+                var parseFile = new Parse.File(name, file);
+                parseFile.save().then(function () {
+                    _this.$rootScope.currentUser.set('icon', parseFile);
+                    _this.$rootScope.currentUser.set('iconUrl', parseFile.url());
+                    return _this.$rootScope.currentUser.save(profInput);
+                }).then(function () {
+                    var profilePhoto = _this.$rootScope.currentUser.get('icon');
+                    _this.$scope.$apply(function () {
+                        fileSelector.fileinput('clear');
+                        _this.profile.icon = profilePhoto.url();
+                        _this.showIcon = true;
+                    });
+                    _this.cfpLoadingBar.complete();
+                    _this.disableInput = false;
+                }, function (error) {
+                    console.error(error);
+                    this.cfpLoadingBar.complete();
+                    this.disableInput = false;
+                });
+            }
+            else {
+                this.$rootScope.currentUser.save(profInput)
+                    .then(function (user) {
+                    console.log('ほぞんしました');
+                    _this.cfpLoadingBar.complete();
+                    _this.disableInput = false;
+                }, function (error) {
+                    console.log('Error: ' + error.code + ' ' + error.message);
+                    _this.cfpLoadingBar.complete();
+                    _this.disableInput = false;
+                });
+            }
         };
         return SettingsProfileController;
     })();
--- src/app/settings/content/profile/settings.profile.controller.ts
+++ src/app/settings/content/profile/settings.profile.controller.ts
@@ -9,10 +9,13 @@ module chavo {
     public profile: Profile;
     public showIcon: boolean = false;
 
+    public disableInput: boolean = false;
+
     /* @ngInject */
     constructor (
       public $scope: ISettingsMenu,
-      public $rootScope: IChavoRootScope) {
+      public $rootScope: IChavoRootScope,
+      public cfpLoadingBar: any) {
 
         this.profile = new Profile(
             $rootScope.currentUser.getUsername(),
@@ -25,59 +28,62 @@ module chavo {
         // ファイルインプットの設定
         var fileSelector: any = angular.element('#photo-selector');
         fileSelector.fileinput({
-          'uploadUrl': 'dummy',
-          'dropZoneEnabled': false,
-          'showCaption': false,
-          'showPreview': true,
-          'browseClass': 'photo-browse-btn btn btn-primary btn-outline',
-          'browseLabel': '画像選択'
+          dropZoneEnabled: false,
+          showCaption: false,
+          showUpload: false,
+          showPreview: true,
+          browseClass: 'photo-browse-btn btn btn-primary btn-outline',
+          browseLabel: '画像選択'
         });
 
-        // アップロード直前のイベントでParseに送る。（bootstrap-file-inputによるアップロードは失敗する。）
-        fileSelector.on('filepreupload', (event: any, data: any, previewId: number, index: number) => {
-
-          if (data.files.length > 0) {
-            var file = data.files[0];
-            var name = 'photo.jpg';
-
-            var parseFile = new Parse.File(name, file);
-            parseFile.save().then(() => {
-
-              this.$rootScope.currentUser.set('icon', parseFile);
-              this.$rootScope.currentUser.set('iconUrl', parseFile.url());
-
-              return this.$rootScope.currentUser.save();
-            }).then(() => {
-
-              var profilePhoto = this.$rootScope.currentUser.get('icon');
-
-              $scope.$apply(() => {
-                this.profile.icon = profilePhoto.url();
-                this.showIcon = true;
-              });
-
-              // プレビュー消す
-              fileSelector.fileinput('clear');
-
-            }, function(error: Parse.Error) {
-              console.error(error);
-            });
-          }
-        });
-
-
-        // bootstrap-file-inputによるアップロードは失敗する。
-        fileSelector.on('fileuploaderror', function(event: any, data: any, previewId: number, index: number) {
-          // エラーメッセージは非表示
-          angular.element('.file-error-message').css('display', 'none');
-
-          console.error('file upload error. (this is expected results. please ignore.)');
-        });
+        // // アップロード直前のイベントでParseに送る。（bootstrap-file-inputによるアップロードは失敗する。）
+        // fileSelector.on('filepreupload', (event: any, data: any, previewId: number, index: number) => {
+        //
+        //   if (data.files.length > 0) {
+        //     var file = data.files[0];
+        //     var name = 'photo.jpg';
+        //
+        //     var parseFile = new Parse.File(name, file);
+        //     parseFile.save().then(() => {
+        //
+        //       this.$rootScope.currentUser.set('icon', parseFile);
+        //       this.$rootScope.currentUser.set('iconUrl', parseFile.url());
+        //
+        //       return this.$rootScope.currentUser.save();
+        //     }).then(() => {
+        //
+        //       var profilePhoto = this.$rootScope.currentUser.get('icon');
+        //
+        //       $scope.$apply(() => {
+        //         this.profile.icon = profilePhoto.url();
+        //         this.showIcon = true;
+        //       });
+        //
+        //       // プレビュー消す
+        //       fileSelector.fileinput('clear');
+        //
+        //     }, function(error: Parse.Error) {
+        //       console.error(error);
+        //     });
+        //   }
+        // });
+        //
+        //
+        // // bootstrap-file-inputによるアップロードは失敗する。
+        // fileSelector.on('fileuploaderror', function(event: any, data: any, previewId: number, index: number) {
+        //   // エラーメッセージは非表示
+        //   angular.element('.file-error-message').css('display', 'none');
+        //
+        //   console.error('file upload error. (this is expected results. please ignore.)');
+        // });
 
 
     }
 
     public saveProfile() {
+
+      this.disableInput = true;
+      this.cfpLoadingBar.start();
 
       var profInput: { username: string; email: string; password?: string } = {
         username: this.profile.username,
@@ -88,12 +94,56 @@ module chavo {
         profInput.password = this.profile.password;
       }
 
-      this.$rootScope.currentUser.save(profInput)
-      .then((user: Parse.User) => {
-        console.log('ほぞんしました');
-      }, (error: Parse.Error) => {
-        console.log('Error: ' + error.code + ' ' + error.message);
-      });
+      var fileSelector: any = angular.element('#photo-selector');
+      if (fileSelector[0].files.length > 0) {
+        // 画像が選択されている場合、保存してユーザと紐付ける。
+
+        var file = fileSelector[0].files[0];
+        var name = 'photo.jpg';
+
+        var parseFile = new Parse.File(name, file);
+        parseFile.save().then(() => {
+
+          this.$rootScope.currentUser.set('icon', parseFile);
+          this.$rootScope.currentUser.set('iconUrl', parseFile.url());
+
+          return this.$rootScope.currentUser.save(profInput);
+        }).then(() => {
+
+          var profilePhoto = this.$rootScope.currentUser.get('icon');
+
+          this.$scope.$apply(() => {
+            // プレビュー消す
+            fileSelector.fileinput('clear');
+
+            this.profile.icon = profilePhoto.url();
+            this.showIcon = true;
+          });
+
+          this.cfpLoadingBar.complete();
+          this.disableInput = false;
+
+        }, function(error: Parse.Error) {
+          console.error(error);
+          this.cfpLoadingBar.complete();
+          this.disableInput = false;
+        });
+      } else {
+        // 画像が選択されていない場合、プロフィールのみ保存
+
+        this.$rootScope.currentUser.save(profInput)
+        .then((user: Parse.User) => {
+          console.log('ほぞんしました');
+          this.cfpLoadingBar.complete();
+          this.disableInput = false;
+        }, (error: Parse.Error) => {
+          console.log('Error: ' + error.code + ' ' + error.message);
+          this.cfpLoadingBar.complete();
+          this.disableInput = false;
+        });
+      }
+
+
 
     }
 
